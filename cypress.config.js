7251b753e2c6c6963e3bf392307848e971a198dc
Andrii Stepura <andriistepura@gmail.com>
2023-11-25 21:25:07 +0000

    add incognito

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,14 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        /* Set incognito mode for chrome browser */
+        if (browser.family === 'chromium' && browser.name !== 'electron') {
+          launchOptions.args.push("--incognito")
+          return launchOptions
+        }
+        /* // end of incognito mode chrome browser config */
+      })
     },
   },
 });
