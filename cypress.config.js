1f7244238e18187094211047c78979597682e9a3
Andrii Stepura <andriistepura@gmail.com>
2023-11-26 01:23:00 +0000

    Add Cucumber

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require("cypress");
+const cucumber = require("cypress-cucumber-preprocessor").default;
 
 module.exports = defineConfig({
   e2e: {
+    specPattern: "**/*.feature", // for Cucumber
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on("file:preprocessor", cucumber()); // for Cucumber
       on('before:browser:launch', (browser = {}, launchOptions) => {
         /* Set incognito mode for chrome browser */
         if (browser.family === 'chromium' && browser.name !== 'electron') {
